f75c2adf3f6ce4ca002f5c68eebdc02c08031488
Serch <silurio@live.com>
2023-07-12 14:23:29 -0600

    refactor(itinerario_data_getter): Se condiciona la generación del CSV a la obtención de URLs nuevas

--- itinerario_data_getter.py
+++ itinerario_data_getter.py
@@ -2,5 +2,6 @@ from web_scrapping_class import WebScrapping
 
 instanceWS = WebScrapping()
 instanceWS.request_n_download()
-instanceWS.get_urls_data()
-instanceWS.from_json_to_csv()
+continuar = instanceWS.get_urls_data()
+if continuar:
+    instanceWS.from_json_to_csv()

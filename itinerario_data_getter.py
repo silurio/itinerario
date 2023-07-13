868099bb91f49836f3b476d4d99a708da02a779e
Serch <silurio@live.com>
2023-07-13 15:04:15 -0600

    refactor(web_scrapping_class, itinerario_data_getter): Se agregó método generación masiva de URLs y obtener_info_restaurante ahora también obtiene las coordenadas de los restaurantes

--- itinerario_data_getter.py
+++ itinerario_data_getter.py
@@ -3,5 +3,6 @@ from web_scrapping_class import WebScrapping
 instanceWS = WebScrapping()
 instanceWS.request_n_download()
 instanceWS.get_urls_data()
-instanceWS.obtener_estado_restaurante()
+instanceWS.obtener_info_restaurante()
+instanceWS.generacion_masiva_urls()
 instanceWS.from_json_to_csv()

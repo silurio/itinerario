f62212360410ee93feead072362bfe7fc47b982e
Serch <silurio@live.com>
2023-07-20 17:49:16 -0600

    fix(restaurantes.json/csv): Se corrige la ubicación del restaurante Salvaxe

--- itinerario_data_getter.py
+++ itinerario_data_getter.py
@@ -1,9 +1,9 @@
 from web_scrapping_class import WebScrapping
 
 instanceWS = WebScrapping()
-# instanceWS.request_n_download()
-# instanceWS.get_urls_data()
-# instanceWS.obtener_info_restaurante()
-# instanceWS.generacion_masiva_urls()
+instanceWS.request_n_download()
+instanceWS.get_urls_data()
+instanceWS.obtener_info_restaurante()
+instanceWS.generacion_masiva_urls()
 # instanceWS.corrector_sitio('SALVAXE')
 instanceWS.from_json_to_csv()

cc974885d6e0383c1c8090f20718f0b3e8da6f67
Serch <silurio@live.com>
2023-08-15 22:34:25 -0600

    fix(restaurantes.json/csv): Se corrige información de restaurantes

--- itinerario_data_getter.py
+++ itinerario_data_getter.py
@@ -5,11 +5,7 @@ instanceWS.request_n_download()
 instanceWS.get_urls_data()
 instanceWS.obtener_info_restaurante()
 instanceWS.generacion_masiva_urls()
-# instanceWS.corrector_sitios(["LA CHIVA TORTAS AHOGADAS",
-#                             "DON BLAS CENADURÍA",
-#                             "MAXIMILIANO CANTINA GAY",
-#                             "SALWA'S  GASTRONOMÍA",
-#                             "GUAU TAP",
-#                             "ESQUINA DE BUENOS AIRES"])
+# instanceWS.corrector_sitios(["GALERA",
+#                             "RESTAURANTE AMALIA"])
 # instanceWS.obtener_place_id_direccion()
 instanceWS.from_json_to_csv()

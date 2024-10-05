d5a642da70c5ab07c33e1df14fe48fcc0749a7ab
Serch <silurio@live.com>
2024-10-05 12:37:55 -0600

    feat(web_scrapping_class, restaurantes.csv/json): Se agregan un nuevo método para la adición de nuevos restaurantes y se actualiza la lista de restaurantes

--- itinerario_data_getter.py
+++ itinerario_data_getter.py
@@ -1,12 +1,25 @@
 from web_scrapping_class import WebScrapping
 
 instanceWS = WebScrapping()
-instanceWS.request_n_download()
-instanceWS.get_urls_data()
+# instanceWS.request_n_download()
+# instanceWS.get_urls_data()
 instanceWS.obtener_info_restaurante()
-instanceWS.generacion_masiva_urls()
+# instanceWS.generacion_masiva_urls()
 
-# instanceWS.corrector_sitios(["ARTILIANO7"])
+# instanceWS.corrector_sitios(["La Zarza Oaxaca",
+# "El Rincón de Macondo",
+# "Tacos de hígado Ramírez Moranchel",
+# "El Micky",
+# "Café Lucky",
+# "La Barra de al Lado",
+# "Cacao para todos"])
+# instanceWS.agregar_info_basica_restaurante([
+#     {
+#         "nombre": "",
+#         "video": ""
+#     }
+# ])
 # instanceWS.obtener_place_id_direccion()
 
 instanceWS.from_json_to_csv()
+# Último sitio agregado: Paletería Maya
